998696e42cce0c5b95847f9480384b6eaea2cdd3
MarieBiau <mbiaugui@enssat.fr>
2017-12-05 15:13:03 +0100

    git add .

---
+++ DataAccessLayer/dbDTO/Posts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataAccessLayer.dbDTO
+{
+    public class posts
+    {
+        public int id { get; set; }
+        public int post_type_id { get; set; }
+        public DateTime creation_date { get; set; }
+        public int score { get; set; }
+        public string body { get; set; }
+        public int user_id { get; set; }
+    }
+}
